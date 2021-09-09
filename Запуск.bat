3ff4baee705e7bf2a7adb104640fd3b0cdc95865
ozr-dev <71367764+ozr-dev@users.noreply.github.com>
2021-09-09 11:42:57 +0400

    Update Запуск.bat

--- Запуск.bat
+++ Запуск.bat
@@ -1,2 +1,2 @@
-chcp 1251 && powershell.exe -executionpolicy unrestricted -File .\GetIllegals.ps1 -all all -path .\Work > .\logger.txt
+chcp 1251 && powershell.exe -executionpolicy unrestricted -File .\GetIllegals.ps1 -all all -path .\Work
 pause
